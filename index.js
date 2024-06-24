2517699a245fa666c91fc0d5c19bcd00a77efd41
Justin Nguyen <168696326+justin09948@users.noreply.github.com>
2024-06-24 14:27:09 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
+
+const body = document.body;
+        const checkbox = document.getElementById('menu-toggle');
+
+        checkbox.addEventListener('change', (event) => {
+            if (event.target.checked) {
+                body.classList.add('no-scroll');
+            } else {
+                body.classList.remove('no-scroll');
+            }
+        });
